6876eb091a1f9cbc6216fd4c355b442f9ad05fbd
Lidia3696 <lidia.ing336@gmail.com>
2025-11-05 17:00:07 +0100

    second commit

--- app/src/main/java/com/example/conversorunitatslidiaingles/AlarmaIntent.java
+++ app/src/main/java/com/example/conversorunitatslidiaingles/AlarmaIntent.java
@@ -7,8 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.provider.AlarmClock;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AlarmaIntent extends AppCompatActivity {
+
+    //hora i minuts
+    int hora, minuts;
+    //missatge que te l'alarma
+    String missatge;
+
+    TextInputEditText horaInput, minutInput, missatgeInput;
+    Button crearAlarmaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,59 @@ public class AlarmaIntent extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    //declarar inputs
+    horaInput = findViewById(R.id.inputHora);
+    minutInput = findViewById(R.id.inputMinut);
+    missatgeInput = findViewById(R.id.inputMissatge);
+    crearAlarmaButton = findViewById(R.id.bttSetAlarm);
+
+    crearAlarmaButton.setOnClickListener(v -> getValues());
+    }
+
+    //agafar valors dels textinputs
+    public void getValues() {
+        //comprovar que els inputs no estiguin buits
+        //un toast es un missatge petit que surt a la part inferior de la pantalla
+        String horaStr = horaInput.getText().toString().trim();
+        String minutStr = minutInput.getText().toString().trim();
+        missatge = missatgeInput.getText().toString().trim();
+
+        if (horaStr.isEmpty() || minutStr.isEmpty() || missatge.isEmpty()) {
+            Toast.makeText(this, "Completa tots els camps", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        //comprovar que els numeros e pguin parsejar be (que no hagin escrites lletres)
+        try {
+            missatge = missatgeInput.getText().toString();
+            hora = Integer.parseInt(horaInput.getText().toString());
+            minuts = Integer.parseInt(minutInput.getText().toString());
+        } catch (Exception e) {
+            Toast.makeText(this, "Escriu valors válids", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        crearAlarma(missatge, hora, minuts);
+
+    }
+
+    public void crearAlarma(String message, int hour, int minutes) {
+
+        try {
+            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                    .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                    .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                    .putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+                    .putExtra(AlarmClock.EXTRA_SKIP_UI, false);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 }
--- app/src/main/java/com/example/conversorunitatslidiaingles/WebSearch.java
+++ app/src/main/java/com/example/conversorunitatslidiaingles/WebSearch.java
@@ -1,5 +1,8 @@
 package com.example.conversorunitatslidiaingles;
 
+import android.app.SearchManager;
+import android.content.ActivityNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
 public class WebSearch extends AppCompatActivity {
+
+    TextInputEditText searchBox;
+    Button bttSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,43 @@ public class WebSearch extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        searchBox = findViewById(R.id.textInputLayoutSearch);
+        bttSearch = findViewById(R.id.bttSearch);
+
+        bttSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchTerms = searchBox.getText().toString();
+                if(!searchTerms.equals("")) {
+                    searchNet(searchTerms);
+                }
+            }
+        }
+        );
+
     }
+
+    private void searchNet(String text) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, text);
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+            searchNetCompat(text);
+        }
+    }
+
+    private void searchNetCompat(String text) {
+        try {
+            Uri uri = Uri.parse("http://www.google.com/#q=" + text);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
